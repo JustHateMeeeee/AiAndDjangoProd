694da18d4d9811b36d9770390d836ac53705a271
Ruslan test <118464902+RuSaldoo@users.noreply.github.com>
2023-10-12 22:56:01 +0500

    boв

--- webApp/webApp/settings.py
+++ webApp/webApp/settings.py
@@ -118,10 +118,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/cardFile'
 
-STATICFILES_DIRS = {
-    BASE_DIR / "cardFile/static",
-}
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
